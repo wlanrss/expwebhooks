35d1b52d7d23153848258e5ce5f0ea15262a547c
shunu <shunmugam@gmail.com>
2023-02-07 12:37:02 +0530

    first commit

---
+++ webhook1.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.sendStatus(200);
+});
+
+app.listen(80, () => {
+  console.log('Webhook server is listening on port 80');
+});
