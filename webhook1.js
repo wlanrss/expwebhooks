76e4410ac99c87250fe73fbd3f8509869fcde85c
wlanrss <55341150+wlanrss@users.noreply.github.com>
2023-02-10 18:01:06 +0530

    version 3.0

--- webhook1.js
+++ webhook1.js
@@ -19,11 +19,8 @@ app.listen(3000, () => {
   console.log('Webhook server is listening on port 3000');
 });
 
-console.log("webhook1 started. Version 2.4")
+console.log("webhook1 started. Version 3.0")
 
-curl -L https://github.com/jenkins-x/jx/releases/download/v1.26.1/jx-linux-amd64.tar.gz | tar xzv
-sudo mv jx /usr/local/bin
-jx install
 
 //curl --request POST --url http:/127.0.0.1:3000/webhook --header 'Content-Type: application/json' --data '{"param1":"param1","param2":"param2"}'
 //>curl --request POST --url "https://webhook1-lyyx.onrender.com:3000/webhook" --header "Content-Type: application/json" --data "{\"param1\":\"param1\",\"param2\":\"two\"}"
